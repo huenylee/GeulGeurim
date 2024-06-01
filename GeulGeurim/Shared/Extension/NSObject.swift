053f0d47d1ba970442b3a96562ad1e05a71fffd4
Hunhee Lee <hunhee.dev@gmail.com>
2024-06-01 22:36:33 +0900

    [feat] Add NSObject extension

---
+++ GeulGeurim/Shared/Extension/NSObject.swift
@@ -0,0 +1,12 @@
+//
+//  NSObject.swift
+//  GeulGeurim
+//
+//  Created by HUNHEE LEE on 28.05.2024.
+//
+
+import Foundation
+
+extension NSObject {
+  static var identifier: String { Self.description() }
+}
